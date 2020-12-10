172db09c75c865b163bc26127e9384a5d29429a3
Marcin Narloch <marcin.narloch@qbituniverse.com>
2020-12-10 22:51:08 +0000

    - init

---
+++ .cicd/setup.sh
@@ -0,0 +1,18 @@
+# Docker Compose [Development]
+# start up
+docker-compose -f docker-compose.Development.yaml up
+
+# take down
+docker-compose -f docker-compose.Development.yaml down
+
+
+# Docker Compose [Production]
+# start up
+docker-compose -f docker-compose.Production.yaml up
+
+# take down
+docker-compose -f docker-compose.Production.yaml down
+
+
+# Docker Run from Image
+# TBC
